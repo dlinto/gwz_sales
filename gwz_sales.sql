838bb0e0d157c440f8d7556f68fd9f8321247364
dlinto <dlinto@outlook.com>
2024-12-09 13:48:05 -0500

    rounding_values
    Rounding turnover & purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,3 +1,3 @@
-SELECT date_date, sum(turnover) AS turnover
+SELECT date_date, ROUND(sum(turnover), 2) AS turnover, ROUND(sum(purchase_cost), 2) AS purchase_cost
 FROM `data-starlight-442202-u6.course16.gwz_sales`
 GROUP BY date_date
